232dd4879eb3f7ba34d0b7058622d8c71e947c07
Prahlad Singh Aswal <91825078+Prahladaswal27@users.noreply.github.com>
2024-07-03 22:27:24 +0530

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -14,5 +14,16 @@ int main()
 
     pair<int, int> p2 = p;
     cout << p2.first << " " << p2.second << endl;
+
+    pair<int, int> &p3 = p;
+
+    cout << "Values Updated" << endl;
+    cout << p.first << " " << p.second << endl;
+    cout << p3.first << " " << p3.second << endl;
+    p3.second = 44;
+    cout << p3.first << " " << p3.second << endl;
+    cout << p.first << " " << p.second << endl;
+
+    cout << newpair.first << " " << newpair.second << endl;
     return 0;
 }
