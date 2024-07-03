b7fa9d76d880cdbbc1142b0c4d1656f7325b0039
Prahlad Singh Aswal <91825078+Prahladaswal27@users.noreply.github.com>
2024-07-03 22:26:44 +0530

    Update List.cpp

--- List.cpp
+++ List.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    pair<int, int> p;
+    p.first = 1;
+    p.second = 2;
+    pair<int, string> newpair = make_pair(100, "Name");
+    cout << p.first << " " << p.second << endl;
+
+    pair<int, int> p1 = {3, 4};
+    cout << p1.first << " " << p1.second << endl;
+
+    pair<int, int> p2 = p;
+    cout << p2.first << " " << p2.second << endl;
+    return 0;
+}
